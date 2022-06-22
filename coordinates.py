a01f307a27bf307afc690570c67076b6f921f01d
bingle-a81 <breath44@yandex.ru>
2022-06-22 22:10:57 +0300

    22.06

--- coordinates.py
+++ coordinates.py
@@ -1,4 +1,19 @@
 from typing import NamedTuple
+from typing import Union
+from typing import Literal
+from dataclasses import dataclass
+
+@dataclass
+class Coordinates1:
+    longitude: float
+    latitude: float
+
+def get_gps_coordinates3() -> Coordinates1:
+    return Coordinates1(10, 20)
+
+print(get_gps_coordinates3().latitude)
+print(get_gps_coordinates3().longitude)
+
 
 class Coordinates(NamedTuple):
     latitude: float
@@ -6,4 +21,16 @@ class Coordinates(NamedTuple):
 
 def get_gps_coordinates() -> Coordinates:
     """Returns current coordinates using MacBook GPS"""
-    return Coordinates(10, 20)
+    return Coordinates(10,20)
+
+a= Union[int, str, float]
+
+# def get_gps_coordinates1() ->dict[Literal["longitude" , "latitude"], float]:
+#     return {"longitude": 10, "latitude": 20}
+coordinates = get_gps_coordinates()
+print(coordinates.latitude)
+print(coordinates.longitude)
+
+# print(
+#     get_gps_coordinates()["longitudeRRR"]  # Тут IDE покажет ошибку!
+# )
